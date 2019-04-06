e3e026e7b2b7ffe8fbe5879db3b23f761263a1d2
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2019-04-06 12:52:42 +0500

    Ready to be deployed

--- server/index.js
+++ server/index.js
@@ -7,30 +7,34 @@ const formData = require('express-form-data')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
-// const router = require('./routes/routerMySQL')
-const router = require('./routes/routerMongo')
-
-//SQL connection
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'shoaib96',
-//   multipleStatements: true
-// })
-
-//MongoDB Setup
-// mongoose.Promise = global.Promise
-mongoose
-  .connect(config.MONGODB_URL, { useNewUrlParser: true })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err))
 
 //App Setup and Middlewares
 app.use(morgan('combined'))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(formData.parse())
-router(app)
+
+let router
+if (process.env.NODE_ENV === 'production') {
+  router = require('./routes/routerMongo')
+  //MongoDB Setup
+  // mongoose.Promise = global.Promise
+  mongoose
+    .connect(config.MONGODB_URL, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err))
+  router(app)
+} else {
+  router = require('./routes/routerMySQL')
+  // SQL connection
+  const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'shoaib96',
+    multipleStatements: true
+  })
+  router(app, db)
+}
 
 app.listen(process.env.PORT || 3050, () => {
   console.log('Listening on 3050')
