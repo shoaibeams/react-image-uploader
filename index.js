36265b6066be3cdb3ff0011b2e7188b3a715c89b
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2019-04-06 13:32:42 +0500

    Ready to deployed

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'))
+  app.use(express.static('client/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
