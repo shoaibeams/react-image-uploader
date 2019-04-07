f613b647e9067bc804cb747960fa1055920b1fbe
Shoaib Khalid <shoaibkhalid1996@gmail.com>
2019-04-07 11:01:34 +0500

    Fixing route issue

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'client', 'build', 'index.html')))
+  app.use(express.static(path.join(__dirname, 'client', 'build')))
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
